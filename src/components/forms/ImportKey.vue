c0196c9317ae300cc60a15f77787ee52176648cf
catsigma <im@catsigma.com>
2019-03-08 18:24:54 +0800

    add key scheme validation

--- src/components/forms/ImportKey.vue
+++ src/components/forms/ImportKey.vue
@@ -1,14 +1,14 @@
 <template>
   <b-form>
     <b-form-group 
-      :label="lang.form_import_label" 
-      :description="lang.form_import_desc"
-      invalid-feedback="Invalid"
-      valid-feedback="Valid">
+      :label="lang.form_import.label" 
+      :description="lang.form_import.desc"
+      :invalid-feedback="lang.form_import.user_key_invalid + key_type"
+      :valid-feedback="lang.form_import.user_key_valid + key_type">
       <b-form-input id="user-key" v-model="user_key" :state="user_key_validation"/>
     </b-form-group>
 
-    <b-form-group :label="lang.form_import_pwd_label" :description="lang.form_import_pwd_desc">
+    <b-form-group v-if="pwd_required" :label="lang.form_import.pwd_label" :description="lang.form_import.pwd_desc">
       <b-form-input  />
     </b-form-group>
 
@@ -25,7 +25,9 @@ export default {
   data() {
     return {
       lang,
-      user_key: ''
+      user_key: '',
+      key_type: '',
+      pwd_required: false
     }
   },
   watch: {
@@ -34,13 +36,64 @@ export default {
   },
   computed: {
     user_key_validation() {
-      try {
-        if (!this.user_key)
-          return null
+      const user_key = this.user_key.trim()
 
-        const prefix = TBC.codec.bs58checkPrefixPick(this.user_key)
+      if (!user_key)
+        return null
+
+      try {
+
+        const prefix = TBC.codec.bs58checkPrefixPick(user_key)
+        this.key_type = prefix.name
+
+        const scheme_require_pwd = new Set([
+          'ed25519_encrypted_seed', 'secp256k1_encrypted_secret_key',
+          'p256_encrypted_secret_key'
+        ])
+        const scheme_not_require_pwd = new Set([
+          'ed25519_secret_key', 'ed25519_seed', 
+          'secp256k1_secret_key', 'p256_secret_key'
+        ])
+
+        if (scheme_require_pwd.has(prefix.name))
+          this.pwd_required = true
+        else if (scheme_not_require_pwd.has(prefix.name)) 
+          this.pwd_required = false
+        else {
+          return false
+        }
+
         return true
-      } catch {}
+        
+      } catch(e) {
+
+        try {
+          const faucet = JSON.parse(user_key)
+          if (faucet.mnemonic && faucet.password && faucet.secret) {
+            const key = TBC.crypto.getKeyFromWords(faucet.mnemonic, faucet.password)
+            this.key_type = lang.key.faucet
+            this.pwd_required = false
+            return true
+          }
+
+        } catch(e) {
+
+          try {
+
+            if (user_key.split(/\s+/g).length >= 12) {
+              TBC.crypto.getKeyFromWords(user_key)
+              this.key_type = lang.key.mnenomic
+              this.pwd_required = true
+              return true
+            }
+
+          } catch {}
+
+        }
+      }
+
+      this.pwd_required = false
+      this.key_type = lang.key.no_scheme
       return false
     }
   }
--- src/langs/en-US.js
+++ src/langs/en-US.js
@@ -1,7 +1,16 @@
 export default {
   please_input_password: 'Please input your password',
-  form_import_pwd_label: 'Password:',
-  form_import_pwd_desc: 'Input the password to decrypt your key',
-  form_import_label: 'Your key:',
-  form_import_desc: 'Accept mnenomic words / Any key with prefix of `edsk`, `spsk`, `p2sk`, `edesk`, `spesk` or `p2esk` / Faucet JSON string for testnet'
+  key: {
+    no_scheme: 'no scheme',
+    mnenomic: 'mnenomic words',
+    faucet: 'faucet'
+  },
+  form_import: {
+    user_key_invalid: 'Invalid key detected: ',
+    user_key_valid: 'Valid key detected: ',
+    pwd_label: 'Password:',
+    pwd_desc: 'Input the password to decrypt your key',
+    label: 'Your key:',
+    desc: 'Accept mnenomic words / Any key with prefix of `edsk`, `spsk`, `p2sk`, `edesk`, `spesk` or `p2esk` / Faucet JSON string for testnet'
+  }
 }
