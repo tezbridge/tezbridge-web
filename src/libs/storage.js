76085b5539229d6c5b2957030649a629a4d8cc4c
catsigma <im@catsigma.com>
2019-04-01 18:53:43 +0800

    add setActiveManager

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -1,5 +1,7 @@
 // @flow
 
+import TBC from 'tezbridge-crypto'
+
 const get = key => localStorage.getItem(key)
 const set = (k, v) => localStorage.setItem(k, v + '')
 
@@ -11,16 +13,42 @@ type Manager = {
 class Storage {
   version : number
   managers : Array<Manager>
+  active_manager: {enc: string, address: string}
   settings : {[string]: Object}
+
 
   constructor() {
     const version = get('version') || '1'
     const managers = get('managers') || '[]'
     const settings = get('settings') || '{}'
+    const active_manager = get('active_manager') || '{"enc": "", "address": ""}'
 
     this.version = parseInt(version)
     this.managers = JSON.parse(managers)
     this.settings = JSON.parse(settings)
+    this.active_manager = JSON.parse(active_manager)
+  }
+
+  setActiveManager(box : TBC.crypto.EncryptedBox) {
+    const password = TBC.codec.toHex(TBC.crypto.genRandomBytes(8))
+
+    return box.revealKey('', password)
+    .then(key => {
+      box.show()
+      .then(enc => {
+        this.active_manager = {
+          address: key.address.slice(0, 4) + '********' + key.address.slice(-4),
+          enc
+        }
+        set('active_manager', JSON.stringify(this.active_manager))
+        box.reveal(password, '')
+        return Promise.resolve(true)
+      })
+    })
+  }
+
+  useActiveManager() {
+
   }
 
   addManager(manager : Manager) {
