fa33a2de060423a7ad88ad76335561ad27efb35e
catsigma <im@catsigma.com>
2019-05-16 16:50:50 +0800

    adding switcher component

---
+++ src/components/Switcher.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div @click="click(v)" :class="{item: true, active: value === v}" v-for="(v, k) in data">
+      {{k}}
+    </div>
+  </div>
+</template>
+
+<script>
+// @flow
+
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: {}
+    },
+    value: String
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    click(item : string) {
+      this.$emit('input', item)
+    }
+  }
+}
+</script>
+
+<style scoped>
+div.item {
+  display: inline-flex;
+  line-height: 0.8rem;
+  font-size: 0.8rem;
+  padding: 2px 4px;
+  border: 1px solid #999;
+  box-shadow: 0 2px 0 0 #ddd;
+}
+div.item.active {
+  box-shadow: none;
+  border: 1px solid #555;
+  background: #eee;
+  transform: translate(0, 1px);
+}
+</style>
