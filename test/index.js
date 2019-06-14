5532a9e4c7409baae582f18a62561686cc351281
catsigma <im@catsigma.com>
2019-06-14 16:32:38 +0800

    remove unused test

--- test/index.js
+++ test/index.js
@@ -1,9 +1,5 @@
 // @flow
 
-import TezBridgeCrypto from 'tezbridge-crypto'
-import { network_client } from '../src/libs/network'
-import { assert, RPCFn } from './util'
-
 const main = async () => {
 }
 
