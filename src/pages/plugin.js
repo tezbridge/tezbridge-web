0165ad50517a005832821ccabba727b8ad8630fc
catsigma <im@catsigma.com>
2020-03-25 08:21:47 +0000

    feature(plugin): adding reuse for prev opened tezbridge

--- src/pages/plugin.js
+++ src/pages/plugin.js
@@ -1,53 +1,70 @@
 // @flow
 
-(() => {
-  const host = 'https://www.tezbridge.com'
-  // const host = 'https://minivm:1234'
+const host = 'https://www.tezbridge.com'
+// const host = 'https://minivm:1234'
 
-  const arg_templates = {
-    KT1_to_default(address : string, amount : string) {
-      return [{"prim":"PUSH","args":[{"prim":"address"},{"string":address}]},{"prim":"CONTRACT","args":[{"prim":"unit"}]},{"prim":"IF_NONE","args":[[{"prim":"PUSH","args":[{"prim":"string"},{"string":"no %default entrypoint"}]},{"prim":"FAILWITH"}],[{"prim":"NIL","args":[{"prim":"operation"}]},[{"prim":"DIP","args":[[{"prim":"DUP"}]]},{"prim":"SWAP"}],{"prim":"PUSH","args":[{"prim":"mutez"},{"int":amount+''}]},{"prim":"UNIT"},{"prim":"TRANSFER_TOKENS"},{"prim":"CONS"},{"prim":"DIP","args":[[{"prim":"DROP"}]]}]]},{"prim":"DIP","args":[[{"prim":"DROP"}]]}]
-    }
+const arg_templates = {
+  KT1_to_default(address : string, amount : string) {
+    return [{"prim":"PUSH","args":[{"prim":"address"},{"string":address}]},{"prim":"CONTRACT","args":[{"prim":"unit"}]},{"prim":"IF_NONE","args":[[{"prim":"PUSH","args":[{"prim":"string"},{"string":"no %default entrypoint"}]},{"prim":"FAILWITH"}],[{"prim":"NIL","args":[{"prim":"operation"}]},[{"prim":"DIP","args":[[{"prim":"DUP"}]]},{"prim":"SWAP"}],{"prim":"PUSH","args":[{"prim":"mutez"},{"int":amount+''}]},{"prim":"UNIT"},{"prim":"TRANSFER_TOKENS"},{"prim":"CONS"},{"prim":"DIP","args":[[{"prim":"DROP"}]]}]]},{"prim":"DIP","args":[[{"prim":"DROP"}]]}]
+  }
+}
+
+function openSigner() {
+  const signer = window.open('', 'TezBridge')
+
+  try {
+    if (signer.location.href === 'about:blank')
+      signer.location.href = `${host}/index.html?signer`
+
+    return {signer, is_prev: false}
+  } catch (e) {
+    return {signer, is_prev: true}
+  }
+}
+
+class TezBridge {
+  signer : window
+  rejects : {[number] : ( ..._ : any) => any}
+  resolves : {[number] : ( ..._ : any) => any}
+  txid: number
+  templates : Object
+
+  mode : string
+  constructor() {
+    this.rejects = {}
+    this.resolves = {}
+    this.txid = 0
+    this.mode = 'local'
+    this.templates = arg_templates
+
+    window.addEventListener('message', e => {
+      if (e.source !== this.signer || !e.data.tezbridge) 
+        return false
+
+      if (e.data.mode) {
+        this.mode = e.data.mode
+      } else if (e.data.error) {
+        this.rejects[e.data.tezbridge](e.data.error)
+      } else {
+        this.resolves[e.data.tezbridge](e.data.result)
+      }
+
+      delete this.rejects[e.data.tezbridge]
+      delete this.resolves[e.data.tezbridge] 
+    })
   }
 
-  class TezBridge {
-    signer : window
-    rejects : {[number] : ( ..._ : any) => any}
-    resolves : {[number] : ( ..._ : any) => any}
-    txid: number
-    templates : Object
-
-    mode : string
-    constructor() {
-      this.rejects = {}
-      this.resolves = {}
-      this.txid = 0
-      this.mode = 'local'
-      this.templates = arg_templates
-
-      window.addEventListener('message', e => {
-        if (e.source !== this.signer || !e.data.tezbridge) 
-          return false
-
-        if (e.data.mode) {
-          this.mode = e.data.mode
-        } else if (e.data.error) {
-          this.rejects[e.data.tezbridge](e.data.error)
-        } else {
-          this.resolves[e.data.tezbridge](e.data.result)
-        }
-
-        delete this.rejects[e.data.tezbridge]
-        delete this.resolves[e.data.tezbridge] 
-      })
+  ready() {
+    if (this.signer && !this.signer.closed) {
+      return Promise.resolve()
     }
+    else {
+      const {signer, is_prev} = openSigner()
+      this.signer = signer
 
-    ready() {
-      if (this.signer && !this.signer.closed) {
+      if (is_prev)
         return Promise.resolve()
-      }
-      else {
-        this.signer = window.open(`${host}/index.html?signer`)
+      else  
         return new Promise(resolve => {
           const fn = (e) => {
             if (e.source !== this.signer) return false
@@ -56,31 +73,30 @@
           }
           window.addEventListener('message', fn)
         })
-      }
-    }
-
-    async request(param : Object) {
-      await this.ready()
-      
-      if (this.signer) {
-        this.txid++
-
-        if (param.method !== 'get_source' && this.mode === 'local')
-          this.signer.focus()
-
-        return new Promise<void>((resolve, reject) => {
-          this.resolves[this.txid] = resolve  
-          this.rejects[this.txid] = reject
-
-          this.signer.postMessage(Object.assign({}, param, {
-            tezbridge: this.txid
-          }), host)
-        })
-      }
-      else
-        throw 'Signer is not opened'
     }
   }
 
-  window.tezbridge = new TezBridge()
-})()
+  async request(param : Object) {
+    await this.ready()
+
+    if (this.signer) {
+      this.txid++
+
+      if (param.method !== 'get_source' && this.mode === 'local')
+        this.signer.focus()
+
+      return new Promise<void>((resolve, reject) => {
+        this.resolves[this.txid] = resolve  
+        this.rejects[this.txid] = reject
+
+        this.signer.postMessage(Object.assign({}, param, {
+          tezbridge: this.txid
+        }), host)
+      })
+    }
+    else
+      throw 'Signer is not opened'
+  }
+}
+
+window.tezbridge = new TezBridge()
